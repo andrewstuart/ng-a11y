139577c0cd9cfbea14fc2a686f77b6f3e5a176a2
Andrew Stuart <andrew.stuart2@gmail.com>
2016-06-01 12:39:44 -0700

    Update 2016-06-01T19:39:43.048Z

--- README.md
+++
@@ -1,2 +0,0 @@
-# ng-a11y
-An angular module to implement common accessibility behavior
---
+++ js/docs-setup.js
@@ -0,0 +1,77 @@
+NG_DOCS={
+  "sections": {
+    "api": "ng-a11y library"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ng-a11y.directive:a11yModal",
+      "shortName": "a11yModal",
+      "type": "directive",
+      "moduleName": "ng-a11y",
+      "shortDescription": "Appends a global hider with class &#39;a11yModal-hider&#39; (you must",
+      "keywords": "10em 20em 5em a11y-compliant a11ymodal a11ymodal-content a11ymodal-hider api appends attribute backdrop background-color bottom calc called center class clicking consumer content css dialog dialog-specific directive ea element elements esc evaluate example expression false fixed global height hidden hide hider html implement implementation inside interacting key left library method modal-hide modalhide module ng-a11y ng-click optional position preexisting properly rgba style text-align time top unhidden user vertical-align white width world z-index"
+    },
+    {
+      "section": "api",
+      "id": "ng-a11y.directive:onEsc",
+      "shortName": "onEsc",
+      "type": "directive",
+      "moduleName": "ng-a11y",
+      "shortDescription": "Runs an expression on escape keyup.",
+      "keywords": "api button directive escape execute expression keyup ng-a11y onesc pressed runs"
+    },
+    {
+      "section": "api",
+      "id": "ng-a11y.directive:upCaptureTab",
+      "shortName": "upCaptureTab",
+      "type": "directive",
+      "moduleName": "ng-a11y",
+      "shortDescription": "When present on the page, the tabbable elements between",
+      "keywords": "api captured directive elements input ng-a11y selectors tab tabbable upcapturetab"
+    },
+    {
+      "section": "api",
+      "id": "ng-a11y.directive:withService",
+      "shortName": "withService",
+      "type": "directive",
+      "moduleName": "ng-a11y",
+      "shortDescription": "Injects a service into a new scope the DOM element.",
+      "keywords": "aliased angular api bar baz case directive dom element es es2 example exampleservice exampleservicetwo exposed expression foo function html inject injects js module multiple ng-a11y scope service services with-service withservice withserviceexample"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/dist/ng-a11y.js"
+    ],
+    "styles": [],
+    "title": "ng-a11y library",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js",
+    "js/dist/ng-a11y.js"
+  ]
+};
